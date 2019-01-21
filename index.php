f6e4026429e9f0ab9f8565209336d3f952358d3d
vagrant <vagrant@vagrant.vm>
2019-01-21 14:16:15 +0000

    edit euro sign

--- index.php
+++ index.php
@@ -36,25 +36,25 @@ date_default_timezone_set("Europe/Amsterdam");
               <h6 class="my-0">Product name</h6>
               <small class="text-muted">Brief description</small>
             </div>
-            <span class="text-muted">$5</span>
+            <span class="text-muted">€5</span>
           </li>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <div>
               <h6 class="my-0">Second product</h6>
               <small class="text-muted">Brief description</small>
             </div>
-            <span class="text-muted">$3</span>
+            <span class="text-muted">€3</span>
           </li>
           <li class="list-group-item d-flex justify-content-between lh-condensed">
             <div>
               <h6 class="my-0">Third item</h6>
               <small class="text-muted">Brief description</small>
             </div>
-            <span class="text-muted">$2</span>
+            <span class="text-muted">€2</span>
           </li>
           <li class="list-group-item d-flex justify-content-between">
             <span>Total (EUR)</span>
-            <strong>$10</strong>
+            <strong>€10</strong>
           </li>
         </ul>
       </div>
