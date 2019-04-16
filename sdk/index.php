d51622b01c5de160c5dfa4ef6d8625cf4ffaf32c
Liwen Hou <liwen.hou@adyen.com>
2019-04-16 10:54:58 +0800

    update sdk checkout card size

--- sdk/index.php
+++ sdk/index.php
@@ -92,7 +92,7 @@ date_default_timezone_set("Europe/Amsterdam");
       </div>
 
 
-      <div class="col-md-6">
+      <div class="col-md-8">
         <div id="shopperDetails">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
             <span>Checkout</span>
