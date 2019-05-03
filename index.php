ffc0aa7e012ca4b4c744b9fc6d54845697be069e
Liwen Hou <liwen.hou@adyen.com>
2019-05-03 12:14:49 +0800

    echo result

--- index.php
+++ index.php
@@ -181,7 +181,7 @@ date_default_timezone_set("Europe/Amsterdam");
       data: { "callFunc1": "1",
               "shopperReference": shopperID},
       success: function(response) {
-        console.log(response);
+        console.log(response.html);
         $('#paymentWindow').html(response.html);
         var csfSetupObj = {
           rootNode: '.cards-div',
