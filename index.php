1796d9bebed6fb30b619d804dd111d44ac8b055c
Liwen Hou <liwen.hou@adyen.com>
2019-05-03 17:29:04 +0800

    change onchange

--- index.php
+++ index.php
@@ -196,10 +196,19 @@ date_default_timezone_set("Europe/Amsterdam");
         securedFields.onBrand ( function(brandObject){
           $("#cardBrand").attr("src", "assets/img/".concat(brandObject.brandImage));
         });
+
+        console.log(response.details[0]);
+        console.log(response.details);
         const molPayEBankingMY = checkout.create('molpay_ebanking_fpx_MY', {
             details: response.details[0], // The details (issuers) coming from the /paymentMethods api call (type: molpay_ebanking_fpx_MY).
-            onChange: console.log("selected") // Gets triggered once the shopper selects an issuer
+            onChange: handleOnChange // Gets triggered once the shopper selects an issuer
         }).mount('#molpay_ebanking');
+
+        function handleOnChange(state, component) {
+          state.isValid // true or false.
+          state.data
+          console.log(state.data)
+        }
 
       }
     });
