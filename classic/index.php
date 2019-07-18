ee205d8a122212ba11b9cf9048660af455b2cfbb
Liwen Hou <liwen.hou@adyen.com>
2019-07-18 22:14:22 +0800

    fix helper dir

--- classic/index.php
+++ classic/index.php
@@ -116,7 +116,7 @@ date_default_timezone_set("Asia/Singapore");
     var encryptedForm = adyen.encrypt.createEncryptedForm( form, key, options);
     encryptedForm.addCardTypeDetection(options.cardTypeElement);
   </script>
-  <script type="module">
+  <script type="text/javascript">
     import collectBrowserInfo from "./browser";
     import base64Url from "./base64url";
     import createIframe from "./iframe";
