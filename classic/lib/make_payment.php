d3e29ab9b43b41e9a67f5734e55743bc9ad6554f
Liwen Hou <liwen.hou@adyen.com>
2019-07-18 14:55:17 +0800

    remove comma

--- classic/lib/make_payment.php
+++ classic/lib/make_payment.php
@@ -25,7 +25,7 @@ try{
 
     "additionalData" => array(
       "card.encrypted.json" => $_POST['adyen-encrypted-data']，
-      "allow3DS2" => true, 
+      "allow3DS2" => true
       //"allow3DS2" => "true"
     ),
     "threeDS2RequestData" => array(
