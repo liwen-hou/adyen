056e66f5cf8d385c3ce7c2ae7e3ee8735b8813a9
Liwen Hou <liwen.hou@adyen.com>
2019-07-18 22:17:55 +0800

    fix helper dir

--- classic/index.php
+++ classic/index.php
@@ -116,13 +116,7 @@ date_default_timezone_set("Asia/Singapore");
     var encryptedForm = adyen.encrypt.createEncryptedForm( form, key, options);
     encryptedForm.addCardTypeDetection(options.cardTypeElement);
   </script>
-  <script type="text/javascript">
-    import collectBrowserInfo from "./browser";
-    import base64Url from "./base64url";
-    import createIframe from "./iframe";
-    import createForm from "./form";
-    import {validateChallengeWindowSize, getChallengeWindowSize} from "./config.js";
-  </script>
+  <script src="./src/index.js"></script>
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
