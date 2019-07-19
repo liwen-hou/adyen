94901502934473d3955f4ac6ea29dca836b9c2d8
liwen-hou <henryhouliwen@gmail.com>
2019-07-19 16:38:01 +0800

    identifyshopper

--- classic/index.php
+++ classic/index.php
@@ -273,31 +273,6 @@ date_default_timezone_set("Asia/Singapore");
        return iframe;
      };
 
-     const perform3DSDeviceFingerprint = (responseData) =>
-     {
-         const serverTransactionID = responseData.additionalData['threeds2.threeDSServerTransID'];
-         const threeDSMethodURL = responseData.additionalData['threeds2.threeDSMethodURL'];
-         const threedsContainer = document.getElementById('threedsContainer');
-         const dataObj = {
-             threeDSServerTransID : serverTransactionID,
-             threeDSMethodNotificationURL : YOUR_3DS_METHOD_NOTIFICATION_URL
-         };
-         const stringifiedDataObject = JSON.stringify(dataObj);
-         // Encode data
-         const base64URLencodedData = base64Url.encode(stringifiedDataObject);
-         const IFRAME_NAME = 'threeDSMethodIframe';
-
-         // Create hidden iframe
-         const iframe = createIframe(threedsContainer, IFRAME_NAME, '0', '0');
-         // Create a form that will use the iframe to POST data to the threeDSMethodURL
-         const form =  createForm('threedsMethodForm', threeDSMethodURL, IFRAME_NAME, 'threeDSMethodData', base64URLencodedData);
-         threedsContainer.appendChild(form);
-         setTimeout( function () {
-             threedsContainer.removeChild( form );
-         }, 1000 );
-         form.submit();
-     };
-
      var form = document.getElementById('adyen-encrypted-form');
      // See https://github.com/Adyen/CSE-JS/blob/master/Options.md for details on the options to use.
      var key ="10001|BBAD60A5F4A892C824D7313DDFA5200FDB037972332E161F34889AB5F35F00D178855C0747E414991FABBF53775E825DFC2B74AC4BFE4AFBE67405C6BAA39AE22624FE87E44E933DE21022237178F5235E954E17C6397D7922BF00039B722DA3ABC5108EAE9CB65F50D247E441A85D72A90B936B888FCD1B0223DF09354F8CC6345168D197FAE515535A5D4511D42979CEA3BC692BA66FA6E7A4D3649C8BB05F1CDC7193B136C064BA9A78BB1C7FA20B23CDAC6A6534C4F1C6B0B98FFEC0CC8A03667AC75E8AF6C8B03C5AD50A0D9297DADCE3CED1DD6FD472A4E498EBCBEE9C3A51718C5C24697C6A6FC9B40FA089DDFE6DE49473DFBBC9E17ADE00899184A1";
@@ -317,8 +292,35 @@ date_default_timezone_set("Asia/Singapore");
            data: {
              'adyen-encrypted-data':encryptedData
            },
-           success: function(response) {
-             console.log(response);
+           success: function(responseData) {
+             console.log(responseData);
+             JSON.parse(responseData);
+             if(responseData.resultCode == "IdentifyShopper") {
+                 const perform3DSDeviceFingerprint = (responseData) =>
+                 {
+                     const serverTransactionID = responseData.additionalData['threeds2.threeDSServerTransID'];
+                     const threeDSMethodURL = responseData.additionalData['threeds2.threeDSMethodURL'];
+                     const threedsContainer = document.getElementById('threedsContainer');
+                     const dataObj = {
+                         threeDSServerTransID : serverTransactionID,
+                         threeDSMethodNotificationURL : "https://18.138.204.96/classic/lib/notification.php"
+                     };
+                     const stringifiedDataObject = JSON.stringify(dataObj);
+                     // Encode data
+                     const base64URLencodedData = base64Url.encode(stringifiedDataObject);
+                     const IFRAME_NAME = 'threeDSMethodIframe';
+
+                     // Create hidden iframe
+                     const iframe = createIframe(threedsContainer, IFRAME_NAME, '0', '0');
+                     // Create a form that will use the iframe to POST data to the threeDSMethodURL
+                     const form =  createForm('threedsMethodForm', threeDSMethodURL, IFRAME_NAME, 'threeDSMethodData', base64URLencodedData);
+                     threedsContainer.appendChild(form);
+                     setTimeout( function () {
+                         threedsContainer.removeChild( form );
+                     }, 1000 );
+                     form.submit();
+                 };
+             };
            }
          }),
 
@@ -328,6 +330,32 @@ date_default_timezone_set("Asia/Singapore");
      var encryptedForm = adyen.encrypt.createEncryptedForm( form, key, options);
      encryptedForm.addCardTypeDetection(options.cardTypeElement);
 
+     window.addEventListener("message", (e) =>
+     {
+         if(e.origin === "https://18.138.204.96/classic/lib/notification.php"){
+             const eventData = e.data;
+             // IdentifyShopper (3DSMethod) response
+             if(eventData.hasOwnProperty('threeDSCompInd')){
+
+                 // If you haven't already performed the next /authorise3ds2 call from your notification URL this
+                 // represents a good place to initiate the an API request
+                 console.log(eventData);
+                 // authorise3DS2RequestAfterIdentifyingShopper(eventData.threeDSCompInd);
+             }
+
+             // Challenge response
+             if(eventData.hasOwnProperty('transStatus') && eventData.hasOwnProperty('threeDSServerTransID')){
+
+                 // If you haven't already performed the next /authorise3ds2 call from your notification URL this
+                 // represents a good place to initiate the an API request
+                 console.log(eventData);
+                 // authorise3DS2RequestAfterChallenge(eventData.transStatus, eventData.threeDSServerTransID);
+             }
+
+             // Run code to remove the iframe from the '#threedsContainer' element
+             // hideIframe();
+         }
+     });
 
 
      </script>
---
+++ classic/lib/notification.php
@@ -0,0 +1,25 @@
+<?php
+date_default_timezone_set("Asia/Singapore");
+?>
+
+<script type="text/javascript">
+ // For IdentifyShopper (3DSMethod) flow
+ // NOTE: This redirect of the iframe should have happened within 10 seconds of POST-ing the form to the
+ // threeDSMethodURL otherwise threeDSCompInd should be set to 'N'
+
+     const data = {
+         type: 'identifyShopper',
+         threeDSCompInd: 'Y'
+     };
+ // // For ChallengeShopper flow
+ // // NOTE: This redirect of the iframe should have happened within 10 minutes of POST-ing the form
+ // // to the acsURL otherwise transStatus should be set to 'U'
+ // const data = {
+ //         type: 'challengeShopper',
+ //         transStatus: 'Y',
+ // threeDSServerTransID: threeds2.threeDS2ResponseData.threeDSServerTransID
+ //     };
+
+    window.parent.postMessage(data, "https://18.138.204.96/classic/index.php");
+
+</script>
