27c41742a5760f34ff5b162e0791f6f8e1bdada7
liwen-hou <henryhouliwen@gmail.com>
2019-08-26 14:48:46 +0800

    append form

--- classic/lib/3dResult.php
+++ classic/lib/3dResult.php
@@ -11,6 +11,7 @@ try{
   // Generate url
   $url = "https://pal-test.adyen.com/pal/servlet/Payment/v46/authorise3d";
   $request = array(
+    "merchantAccount" => $authentication['merchantAccount'],
     "md" => $_GET["MD"],
     "paResponse" => $_POST['PaRes'],
     "threeDS2RequestData" => array(
