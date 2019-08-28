b4932f847794ab63f527e4ec1f3a1d5271423c94
liwen-hou <henryhouliwen@gmail.com>
2019-08-28 23:27:51 +0800

    pass commission rate

--- MarketPay/index.php
+++ MarketPay/index.php
@@ -24,8 +24,7 @@ date_default_timezone_set("Asia/Singapore");
     <div class="container">
       <div class="py-5 text-center">
         <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h2>Checkout form</h2>
-        <p class="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+        <h2>Adyen MarketPay Demo</h2>
       </div>
 
       <div class="row">
