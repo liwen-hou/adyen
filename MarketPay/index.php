13246c5375081a2a32b800cf9575a24b25c2f2b4
liwen-hou <henryhouliwen@gmail.com>
2019-08-28 15:50:59 +0800

    return url

--- MarketPay/index.php
+++ MarketPay/index.php
@@ -32,45 +32,53 @@ date_default_timezone_set("Asia/Singapore");
         <div class="col-md-4 order-md-2 mb-4">
           <h4 class="d-flex justify-content-between align-items-center mb-3">
             <span class="text-muted">Your cart</span>
-            <span class="badge badge-secondary badge-pill">3</span>
+            <span class="badge badge-secondary badge-pill">2</span>
           </h4>
           <ul class="list-group mb-3">
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Product name</h6>
+                <h6 class="my-0">Marketplace1 Item</h6>
+                <small class="text-muted">Brief description</small>
+              </div>
+              <span class="text-muted">SGD 20</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Marketplace2 Item</h6>
+                <small class="text-muted">Brief description</small>
+              </div>
+              <span class="text-muted">SGD 80</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between lh-condensed">
+              <div>
+                <h6 class="my-0">Commission</h6>
                 <small class="text-muted">Brief description</small>
               </div>
               <span class="text-muted">
-                <select class="custom-select d-block w-2" id="state" required>
+                <select class="custom-select d-block w-2" id="state" required="">
                   <option value="">Choose...</option>
-                  <option>California</option>
+                  <option>5%</option>
+                  <option>10%</option>
                 </select>
               </span>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Second product</h6>
+                <h6 class="my-0">VAT</h6>
                 <small class="text-muted">Brief description</small>
               </div>
-              <span class="text-muted">$8</span>
+              <span class="text-muted">
+                <select class="custom-select d-block w-2" id="state" required="">
+                  <option value="">Choose...</option>
+                  <option>3%</option>
+                  <option>7%</option>
+                </select>
+              </span>
             </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
-            </li>
+
             <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
+              <span>Total (SGD)</span>
+              <strong>100</strong>
             </li>
           </ul>
 
