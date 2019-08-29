3fd540859e6f63ab6928d9638bed07c811718544
liwen-hou <henryhouliwen@gmail.com>
2019-08-29 15:15:47 +0800

    noti

--- MarketPay/signup.php
+++ MarketPay/signup.php
@@ -27,69 +27,95 @@ date_default_timezone_set("Asia/Singapore");
         <h2>Adyen MarketPay Demo</h2>
       </div>
 
-      <div class="row">
-        <div class="col-md-4 order-md-2 mb-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">Your cart</span>
-            <span class="badge badge-secondary badge-pill">2</span>
-          </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Marketplace1 Item</h6>
-                <small class="text-muted">Brief description</small>
+      <div class="col-md-8 order-md-1">
+        <h4 class="mb-3">Billing address</h4>
+        <form class="needs-validation" novalidate>
+          <div class="row">
+            <div class="col-md-6 mb-3">
+              <label for="firstName">First name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Valid first name is required.
               </div>
-              <span class="text-muted">SGD 20</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Marketplace2 Item</h6>
-                <small class="text-muted">Brief description</small>
+            </div>
+            <div class="col-md-6 mb-3">
+              <label for="lastName">Last name</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+              <div class="invalid-feedback">
+                Valid last name is required.
               </div>
-              <span class="text-muted">SGD 80</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Commission</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">
-                <select class="custom-select d-block w-2" id="commission" required="">
-                  <option value="">Choose...</option>
-                  <option value="5">5%</option>
-                  <option value="10">10%</option>
-                </select>
-              </span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">VAT</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">
-                <select class="custom-select d-block w-2" id="vat" required="">
-                  <option value="">Choose...</option>
-                  <option value="3">3%</option>
-                  <option value="7">7%</option>
-                </select>
-              </span>
-            </li>
+            </div>
+          </div>
 
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (SGD)</span>
-              <strong>100</strong>
-            </li>
-          </ul>
+          <div class="mb-3">
+            <label for="username">Username</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">@</span>
+              </div>
+              <input type="text" class="form-control" id="username" placeholder="Username" required>
+              <div class="invalid-feedback" style="width: 100%;">
+                Your username is required.
+              </div>
+            </div>
+          </div>
 
-        </div>
+          <div class="mb-3">
+            <label for="email">Email <span class="text-muted">(Optional)</span></label>
+            <input type="email" class="form-control" id="email" placeholder="you@example.com">
+            <div class="invalid-feedback">
+              Please enter a valid email address for shipping updates.
+            </div>
+          </div>
 
-        <div class="col-md-8 order-md-1">
-          <form class="needs-validation" novalidate>
-            <h4 class="mb-3">Payment</h4>
-            <div id="paymentForm"></div>
-          </form>
-        </div>
+          <div class="mb-3">
+            <label for="address">Address</label>
+            <input type="text" class="form-control" id="address" placeholder="1234 Main St" required>
+            <div class="invalid-feedback">
+              Please enter your shipping address.
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
+            <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+          </div>
+
+          <div class="row">
+            <div class="col-md-5 mb-3">
+              <label for="country">Country</label>
+              <select class="custom-select d-block w-100" id="country" required>
+                <option value="">Choose...</option>
+                <option>United States</option>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid country.
+              </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="state">State</label>
+              <select class="custom-select d-block w-100" id="state" required>
+                <option value="">Choose...</option>
+                <option>California</option>
+              </select>
+              <div class="invalid-feedback">
+                Please provide a valid state.
+              </div>
+            </div>
+            <div class="col-md-3 mb-3">
+              <label for="zip">Zip</label>
+              <input type="text" class="form-control" id="zip" placeholder="" required>
+              <div class="invalid-feedback">
+                Zip code required.
+              </div>
+            </div>
+          </div>
+
+
+          <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+        </form>
       </div>
+
 
       <footer class="my-5 pt-5 text-muted text-center text-small">
         <p class="mb-1">&copy; 2017-2018 Company Name</p>
