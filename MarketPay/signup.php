f885136ed23c6abc220eeb7a0ba726456ee17206
liwen-hou <henryhouliwen@gmail.com>
2019-08-29 17:03:41 +0800

    noti

--- MarketPay/signup.php
+++ MarketPay/signup.php
@@ -61,7 +61,11 @@ date_default_timezone_set("Asia/Singapore");
             <div class="col-md-3 mb-3">
               <label for="zip">Zip</label>
               <input type="text" class="form-control" id="zip" placeholder="" required>
+              <div class="invalid-feedback">
+                Zip code required.
+              </div>
             </div>
+          </div>
 
           <div class="mb-3">
             <label for="email">Email <span class="text-muted">(Optional)</span></label>
@@ -81,35 +85,6 @@ date_default_timezone_set("Asia/Singapore");
             <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
           </div>
 
-          <div class="row">
-            <div class="col-md-5 mb-3">
-              <label for="country">Country</label>
-              <select class="custom-select d-block w-100" id="country" required>
-                <option value="">Choose...</option>
-                <option>United States</option>
-              </select>
-              <div class="invalid-feedback">
-                Please select a valid country.
-              </div>
-            </div>
-            <div class="col-md-4 mb-3">
-              <label for="state">State</label>
-              <select class="custom-select d-block w-100" id="state" required>
-                <option value="">Choose...</option>
-                <option>California</option>
-              </select>
-              <div class="invalid-feedback">
-                Please provide a valid state.
-              </div>
-            </div>
-            <div class="col-md-3 mb-3">
-              <label for="zip">Zip</label>
-              <input type="text" class="form-control" id="zip" placeholder="" required>
-              <div class="invalid-feedback">
-                Zip code required.
-              </div>
-            </div>
-          </div>
 
 
           <button class="btn btn-primary btn-lg btn-block" type="submit">Sign up</button>
