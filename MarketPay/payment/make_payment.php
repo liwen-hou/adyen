1f2f22202fd07711b96806a47f20c1ae1c64c2e0
liwen-hou <henryhouliwen@gmail.com>
2019-08-29 18:45:53 +0800

    noti

--- MarketPay/payment/make_payment.php
+++ MarketPay/payment/make_payment.php
@@ -99,41 +99,41 @@ try{
     ),
   );
   $data = json_encode($request);
-
-  //  Initiate curl
-  $curlAPICall = curl_init();
-
-  // Set to POST
-  curl_setopt($curlAPICall, CURLOPT_CUSTOMREQUEST, "POST");
-
-  // Will return the response, if false it print the response
-  curl_setopt($curlAPICall, CURLOPT_RETURNTRANSFER, true);
-
-  // Add JSON message
-  curl_setopt($curlAPICall, CURLOPT_POSTFIELDS, $data);
-
-  // Set the url
-  curl_setopt($curlAPICall, CURLOPT_URL, $url);
-
-  // Api key
-  curl_setopt($curlAPICall, CURLOPT_HTTPHEADER,
-  array(
-    "X-Api-Key: " . $authentication['checkoutAPIkey'],
-    "Content-Type: application/json",
-    "Content-Length: " . strlen($data)
-    )
-  );
-  // Execute
-  $result = curl_exec($curlAPICall);
-  // Error Check
-  if ($result === false){
-    throw new Exception(curl_error($curlAPICall), curl_errno($curlAPICall));
-  }
-
-  $payment = json_decode($result, true);
-
-  // Closing
-  curl_close($curlAPICall);
+  echo $data;
+  // //  Initiate curl
+  // $curlAPICall = curl_init();
+  //
+  // // Set to POST
+  // curl_setopt($curlAPICall, CURLOPT_CUSTOMREQUEST, "POST");
+  //
+  // // Will return the response, if false it print the response
+  // curl_setopt($curlAPICall, CURLOPT_RETURNTRANSFER, true);
+  //
+  // // Add JSON message
+  // curl_setopt($curlAPICall, CURLOPT_POSTFIELDS, $data);
+  //
+  // // Set the url
+  // curl_setopt($curlAPICall, CURLOPT_URL, $url);
+  //
+  // // Api key
+  // curl_setopt($curlAPICall, CURLOPT_HTTPHEADER,
+  // array(
+  //   "X-Api-Key: " . $authentication['checkoutAPIkey'],
+  //   "Content-Type: application/json",
+  //   "Content-Length: " . strlen($data)
+  //   )
+  // );
+  // // Execute
+  // $result = curl_exec($curlAPICall);
+  // // Error Check
+  // if ($result === false){
+  //   throw new Exception(curl_error($curlAPICall), curl_errno($curlAPICall));
+  // }
+  //
+  // $payment = json_decode($result, true);
+  //
+  // // Closing
+  // curl_close($curlAPICall);
 } catch (Exception $e) {
   trigger_error(sprintf(
     'API call failed with error #%d, %s', $e->getCode(), $e->getMessage()
@@ -144,4 +144,4 @@ try{
 
 
 // When this file gets called by javascript or another language, it will respond with a json object
-echo $result;
+// echo $result;
