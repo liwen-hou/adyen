3b3429170f49eaf7d5cd27e4bb3a074858a4c57f
liwen-hou <henryhouliwen@gmail.com>
2019-08-29 18:18:01 +0800

    noti

--- MarketPay/index.php
+++ MarketPay/index.php
@@ -37,17 +37,21 @@ date_default_timezone_set("Asia/Singapore");
           <ul class="list-group mb-3">
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Marketplace1 Item</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 class="my-0">Marketplace 1 Item</h6>
+                <small class="text-muted">SGD 20</small>
               </div>
-              <span class="text-muted">SGD 20</span>
+              <div class="col-md-6 mb-3">
+                <input type="text" id="accountCode1" placeholder="Account Code">
+              </div>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <h6 class="my-0">Marketplace2 Item</h6>
-                <small class="text-muted">Brief description</small>
+                <h6 class="my-0">Marketplace 2 Item</h6>
+                <small class="text-muted">SGD 80</small>
               </div>
-              <span class="text-muted">SGD 80</span>
+              <div class="col-md-6 mb-3">
+                <input type="text" id="accountCode2" placeholder="Account Code">
+              </div>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
@@ -112,27 +116,5 @@ date_default_timezone_set("Asia/Singapore");
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="./js/main.js"></script>
-    <script>
-      // Example starter JavaScript for disabling form submissions if there are invalid fields
-      (function() {
-        'use strict';
-
-        window.addEventListener('load', function() {
-          // Fetch all the forms we want to apply custom Bootstrap validation styles to
-          var forms = document.getElementsByClassName('needs-validation');
-
-          // Loop over them and prevent submission
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            }, false);
-          });
-        }, false);
-      })();
-    </script>
   </body>
 </html>
