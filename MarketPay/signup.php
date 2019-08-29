b30fbb1e6e1d80c23255650c042f0620bfb9c3b2
liwen-hou <henryhouliwen@gmail.com>
2019-08-29 17:01:35 +0800

    noti

--- MarketPay/signup.php
+++ MarketPay/signup.php
@@ -28,45 +28,44 @@ date_default_timezone_set("Asia/Singapore");
         <p class="lead">New Tenant Signup.</p>
       </div>
 
-      <div class="col-md-8 order-md-1">
-        <h4 class="mb-3">KYC Information</h4>
+      <div class="col-md-12">
         <form class="needs-validation" novalidate>
-          <div class="row">
-            <div class="col-md-6 mb-3">
-              <label for="firstName">First name</label>
-              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Valid first name is required.
-              </div>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="lastName">Last name</label>
-              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Valid last name is required.
-              </div>
+          <h4 class="mb-3">Business Details</h4>
+
+          <div class="mb-3">
+            <label for="username">Legal Business Name</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="legalname" placeholder="Your legal business name" required>
             </div>
           </div>
 
-          <div class="mb-3">
-            <label for="username">Username</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text">@</span>
-              </div>
-              <input type="text" class="form-control" id="username" placeholder="Username" required>
-              <div class="invalid-feedback" style="width: 100%;">
-                Your username is required.
-              </div>
+          <div class="row">
+            <div class="col-md-6 mb-3">
+              <label for="firstName">Share Holder First name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+            </div>
+            <div class="col-md-6 mb-3">
+              <label for="lastName">Share Holder Last name</label>
+              <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
             </div>
           </div>
+
+          <div class="row">
+            <div class="col-md-5 mb-3">
+              <label for="country">Country</label>
+              <select class="custom-select d-block w-100" id="country" required>
+                <option value="">Choose...</option>
+                <option>Singapore</option>
+              </select>
+            </div>
+            <div class="col-md-3 mb-3">
+              <label for="zip">Zip</label>
+              <input type="text" class="form-control" id="zip" placeholder="" required>
+            </div>
 
           <div class="mb-3">
             <label for="email">Email <span class="text-muted">(Optional)</span></label>
             <input type="email" class="form-control" id="email" placeholder="you@example.com">
-            <div class="invalid-feedback">
-              Please enter a valid email address for shipping updates.
-            </div>
           </div>
 
           <div class="mb-3">
