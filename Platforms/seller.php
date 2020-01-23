985e60e6cbfe9496681a99e120b5635367b12671
liwen-hou <henryhouliwen@gmail.com>
2020-01-23 10:57:28 +0800

    change log in

--- Platforms/seller.php
+++ Platforms/seller.php
@@ -49,7 +49,7 @@ date_default_timezone_set("Asia/Singapore");
               <p class="seller-option-text">Sign in if you are already a restaurant with us.
                 <input style="margin-top: 10px;" type="text" class="form-control" id="sellerId" name="sellerId" placeholder="Your unique restaurant ID" required>
                 <input style="margin-top: 10px;" type="text" class="form-control" id="password" name="password" placeholder="Password" required>
-                <button class="btn btn-primary btn-lg btn-block" style="margin-top: 10px;" type="submit">Sign In</button>
+                <button class="btn btn-primary btn-lg btn-block" style="margin: 10px;" type="submit">Sign In</button>
                 <a href="signup.php">Sign up</a>
               </p>
           </div>
