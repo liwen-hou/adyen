67e2515924899c2b134574b4bd1137b6abacdf55
liwen-hou <henryhouliwen@gmail.com>
2020-01-23 14:14:57 +0800

    change log in

--- Platforms/seller.php
+++ Platforms/seller.php
@@ -37,7 +37,7 @@ date_default_timezone_set("Asia/Singapore");
               <h5 class="card-title">Welcome Back</h5>
               <p class="seller-option-text">Sign in if you are already a restaurant with us.
                 <input style="margin-top: 10px;" type="text" class="form-control" id="sellerId" name="sellerId" placeholder="Your unique restaurant ID" required>
-                <input style="margin-top: 10px;" type="text" class="form-control" id="password" name="password" placeholder="Password" required>
+                <input style="margin-top: 10px; margin-bottom: 10px;" type="text" class="form-control" id="password" name="password" placeholder="Password" required>
                 <row>
                   <button style="margin-top: 10px; margin-bottom: 10px;" type="submit">Sign In</button>
                   |<a href="signup.php"> Sign up</a>
--- Platforms/seller_status.php
+++ Platforms/seller_status.php
@@ -158,6 +158,9 @@ date_default_timezone_set("Asia/Singapore");
         html = '<h5 class="card-title">Welcome back '+ response.accountHolderDetails.individualDetails.name.firstName + ' ' + response.accountHolderDetails.individualDetails.name.lastName + '</h5>';
         $('#sellerInfo').append(html);
 
+        html = '<img class="d-block mx-auto mb-4" src="img/happy.svg" alt="" width="40" height="40">';
+        $('#sellerInfo').append(html);
+
         html = '<h6>Account Code: <span class="badge badge-light">' + response.accounts[0].accountCode + '</span></h6>';
         $('#sellerInfo').append(html);
 
