c95dcbba7add3c2d687111cf8532612bc83c5a4d
liwen-hou <henryhouliwen@gmail.com>
2020-01-23 11:41:45 +0800

    change log in

--- Platforms/banner.php
+++ Platforms/banner.php
@@ -1,5 +1,5 @@
 <div class="py-5 text-center">
   <img class="d-block mx-auto mb-4" src="img/adyen.png" alt="" width="72" height="72">
-  <h2><font color="white">Merchant Name POS Portal</font></h2>
-  <h5><font color="white">Powered by Adyen for Platforms</font></h4>
+  <h2><font color="#f8f9fa">Merchant Name POS Portal</font></h2>
+  <h5><font color="#f8f9fa">Powered by Adyen for Platforms</font></h4>
 </div>
