cc257d2c51a3fdbbe462aea034c1d14b1ca33fec
liwen-hou <henryhouliwen@gmail.com>
2020-01-23 11:26:56 +0800

    change log in

--- Platforms/seller.php
+++ Platforms/seller.php
@@ -18,7 +18,7 @@ date_default_timezone_set("Asia/Singapore");
     <link href="form-validation.css" rel="stylesheet">
   </head>
 
-  <body class="bg-light" background="img/bg.jpg">
+  <body class="bg-light" background="img/bg.jpg" style="background-size: 100%;">
     <?php
      require 'header.php';
     ?>
