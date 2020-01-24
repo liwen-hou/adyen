972707a947610641c1efb455034cfe4ee226833e
liwen-hou <henryhouliwen@gmail.com>
2020-01-24 11:35:34 +0800

    update

--- Platforms/management.php
+++ Platforms/management.php
@@ -34,7 +34,7 @@ date_default_timezone_set("Asia/Singapore");
           <h5 class="card-title" style="margin-bottom: 10px;">Choose a restaurant:</h5>
 
           <div class="mb-3">
-            <select class="custom-select d-block w-100" id="sellerId" onchange="this.options[this.selectedIndex].value && (window.location = 'management.php?=sellerId=' + this.options[this.selectedIndex].value);" >
+            <select class="custom-select d-block w-100" id="sellerId" onchange="this.options[this.selectedIndex].value && (window.location = 'management.php?sellerId=' + this.options[this.selectedIndex].value);" >
               <option value="">Choose...</option>
 
               <?php
