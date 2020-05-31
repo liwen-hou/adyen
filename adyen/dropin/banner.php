05a1297aa8b94580387bcbabc3c614b65fa643d0
Liwen <henryhouliwen@gmail.com>
2020-05-31 14:42:26 +0800

    payment methods

--- adyen/dropin/banner.php
+++ adyen/dropin/banner.php
@@ -1,5 +1,5 @@
 <div class="py-5 text-center">
   <img class="d-block mx-auto mb-4" src="img/adyen.png" alt="" width="72" height="72">
-  <h2><font color="#f8f9fa">Merchant Name</font></h2>
-  <h5><font color="#f8f9fa">Powered by Adyen</font></h4>
+  <h2>Merchant Name</h2>
+  <h5>Powered by Adyen</h5>
 </div>
