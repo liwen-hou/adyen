0c24863270f87be7e0272c84a89005fb29fb032e
Liwen <henryhouliwen@gmail.com>
2020-05-31 15:53:28 +0800

    demo folder

--- adyen/demo/banner.php
+++ adyen/demo/banner.php
@@ -1,22 +1,22 @@
 <div class="py-5 text-center">
-  <img data-toggle="modal" data-target="#exampleModal" id="logo" class="d-block mx-auto mb-4" src="https://ga0.imgix.net/logo/o/112563-1519298310-3838809?ixlib=rb-1.0.0&ch=Width%2CDPR&auto=format" alt="" width="72" height="72">
+  <img data-toggle="modal" data-target="#updateLogo" id="logo" class="d-block mx-auto mb-4" src="https://ga0.imgix.net/logo/o/112563-1519298310-3838809?ixlib=rb-1.0.0&ch=Width%2CDPR&auto=format" alt="" width="72" height="72">
   <h2>Merchant Name</h2>
   <h5>Powered by Adyen</h5>
 </div>
 
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="updateLogoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="updateLogoModalLabel">Logo Link</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <textarea class="form-control" style="min-width: 100%"></textarea>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
