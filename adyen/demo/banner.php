6ef2626654ef1a86063f3e1b35532781e6dbc563
Liwen <henryhouliwen@gmail.com>
2020-05-31 20:09:59 +0800

    change logo

--- adyen/demo/banner.php
+++ adyen/demo/banner.php
@@ -4,7 +4,7 @@
 
 <div class="py-5 text-center">
   <img data-toggle="modal" data-target="#updateLogoModal" id="logo" class="d-block mx-auto mb-4" src="https://ga0.imgix.net/logo/o/112563-1519298310-3838809?ixlib=rb-1.0.0&ch=Width%2CDPR&auto=format" alt="" width="72" height="72">
-  <h2 id="merchantName" data-toggle="modal" data-target="#updateMerchantName">Merchant Name</h2>
+  <h2 id="merchantName" data-toggle="modal" data-target="#updateMerchantNameModal">Merchant Name</h2>
   <h5>Powered by Adyen</h5>
 </div>
 
