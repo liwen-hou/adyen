72534ebcb37a9f65bf62effc969bc580bd968344
Liwen <henryhouliwen@gmail.com>
2020-05-31 15:39:46 +0800

    demo folder

--- adyen/demo/banner.php
+++ adyen/demo/banner.php
@@ -1,5 +1,5 @@
 <div class="py-5 text-center">
-  <img class="d-block mx-auto mb-4" src="https://ga0.imgix.net/logo/o/112563-1519298310-3838809?ixlib=rb-1.0.0&ch=Width%2CDPR&auto=format" alt="" width="72" height="72">
+  <img id="logo" class="d-block mx-auto mb-4" src="https://ga0.imgix.net/logo/o/112563-1519298310-3838809?ixlib=rb-1.0.0&ch=Width%2CDPR&auto=format" alt="" width="72" height="72">
   <h2>Merchant Name</h2>
   <h5>Powered by Adyen</h5>
 </div>
