91c08a95968ddef019659303abc803775b8c2905
Liwen <henryhouliwen@gmail.com>
2020-09-14 13:49:22 +0800

    apple pay

--- adyen/dropin/js/main.js
+++ adyen/dropin/js/main.js
@@ -37,7 +37,7 @@ function startPayment(){
     getPaymentMethods().then(paymentMethodsResponse => {
 
       console.log(paymentMethodsResponse);
-      $('#selectPaymentMethods').html('<div id="applepay-container"></div>');
+      $('#selectPaymentMethods').html('<div id="dropin"></div>');
 
       const configuration = {
         locale: "en-GB",
@@ -48,8 +48,19 @@ function startPayment(){
 
       const checkout = new AdyenCheckout(configuration);
 
-      const applepay = checkout.create("applepay", {
-            amount: "2000",
+      const dropin = checkout
+      .create('dropin', {
+        paymentMethodsConfiguration: {
+
+          card: { //Example optional configuration for Cards
+            hasHolderName: true,
+            holderNameRequired: true,
+            enableStoreDetails: true,
+            name: 'Credit or debit card'
+          },
+
+          applepay: { // Required configuration for Apple Pay
+            amount: 2000,
             currencyCode: countryCurrency[countryCode],
             countryCode: countryCode,
             configuration: {
@@ -89,126 +100,63 @@ function startPayment(){
                 }
               });
             }
-      }).mount("#applepay-container");
+          },
 
-      // applepay
-      // .isAvailable()
-      // .then(() => {
-      //   applepay.mount("#applepay");
-      // })
-      // .catch(e => {
-      //   // Apple Pay is not available
-      // // });
-
-      // const dropin = checkout
-      // .create('dropin', {
-      //   paymentMethodsConfiguration: {
-
-      //     card: { //Example optional configuration for Cards
-      //       hasHolderName: true,
-      //       holderNameRequired: true,
-      //       enableStoreDetails: true,
-      //       name: 'Credit or debit card'
-      //     },
-
-      //     applepay: { // Required configuration for Apple Pay
-      //       amount: 2000,
-      //       currencyCode: countryCurrency[countryCode],
-      //       countryCode: countryCode,
-      //       configuration: {
-      //         merchantName: 'Adyen Test merchant', // Name to be displayed on the form
-      //         merchantIdentifier: 'merchant.com.adyen.LiwenHou.test' // Your Apple merchant identifier as described in https://developer.apple.com/documentation/apple_pay_on_the_web/applepayrequest/2951611-merchantidentifier
-      //       },
-      //       onSubmit: (state) => {
-      //         makePayment(state.data)
-      //         .then(paymentResponse => {
-      //           console.log(paymentResponse);
-      //           if (paymentResponse.hasOwnProperty("action")) {
-      //             dropin.handleAction(paymentResponse.action);
-      //           } else {
-
-      //             dropin.setStatus('success', { message: 'Payment successful!' });
-
-      //           }
-      //       // Drop-in will handle the action object from the /payments response
-      //         }).catch(error => {
-      //           throw Error(error);
-      //         });
-
-      //       },
-      //       onValidateMerchant: (resolve, reject, validationURL) => {
-      //         console.log(validationURL);
-
-      //         $.ajax({
-      //           url: "payment/apple_pay.php",
-      //           type: 'post',
-      //           data: {
-      //             "validationURL": validationURL
-      //           },
-      //           success: function(response) {
-      //             response = JSON.parse(response);
-      //             console.log(response);
-      //             resolve(response);
-      //           }
-      //         });
-      //       }
-      //     },
-
-      //     paywithgoogle: { // Example required configuration for Google Pay
-      //       environment: "TEST", // Change this to PRODUCTION when you're ready to accept live Google Pay payments
-      //       configuration: {
-      //         gatewayMerchantId: "LiwenHou", // Your Adyen merchant or company account name
-      //         merchantName: "Liwen Test" // Optional. The name that appears in the payment sheet.
-      //       },
-      //       buttonColor: "white" //Optional. Use a white Google Pay button.
-      //     //For other optional configuration, see section below.
-      //     }
-      //   },
-      //   onChange:(state, dropin) => {
-      //     console.log(state)
-      //   },
-      //   onSubmit: (state, dropin) => {
-      //     makePayment(state.data)
-      //     // Your function calling your server to make the /payments request
-      //     .then(paymentResponse => {
-      //       console.log(paymentResponse);
-      //       if (paymentResponse.hasOwnProperty("action")) {
-      //         dropin.handleAction(paymentResponse.action);
-      //       } else {
-      //         if (paymentResponse.resultCode == "Authorised") {
-      //           dropin.setStatus('success', { message: 'Payment successful!' });
-      //         } else {
-      //           dropin.setStatus('error', { message: 'Something went wrong.'});
-      //         }
-      //       }
-      //       // Drop-in will handle the action object from the /payments response
-      //     })
-      //     .catch(error => {
-      //       throw Error(error);
-      //     });
-      //   },
-      //   onAdditionalDetails: (state, dropin) => {
-      //     makeDetailsCall(state.data)
-      //     // Your function calling your server to make a /payments/details request
-      //     .then(response => {
-      //       console.log(response);
-      //       if (response.hasOwnProperty("action")) {
-      //         dropin.handleAction(response.action);
-      //       } else {
-      //         if (response.resultCode == "Authorised") {
-      //           dropin.setStatus('success', { message: 'Payment successful!' });
-      //         } else {
-      //           dropin.setStatus('error', { message: 'Something went wrong.'});
-      //         }
-      //       }
-      //       // Drop-in will handle the action object from the /payments/details response
-      //     })
-      //     .catch(error => {
-      //       throw Error(error);
-      //     });
-      //   }
-      // })
-      // .mount('#dropin');
+          paywithgoogle: { // Example required configuration for Google Pay
+            environment: "TEST", // Change this to PRODUCTION when you're ready to accept live Google Pay payments
+            configuration: {
+              gatewayMerchantId: "LiwenHou", // Your Adyen merchant or company account name
+              merchantName: "Liwen Test" // Optional. The name that appears in the payment sheet.
+            },
+            buttonColor: "white" //Optional. Use a white Google Pay button.
+          //For other optional configuration, see section below.
+          }
+        },
+        onChange:(state, dropin) => {
+          console.log(state)
+        },
+        onSubmit: (state, dropin) => {
+          makePayment(state.data)
+          // Your function calling your server to make the /payments request
+          .then(paymentResponse => {
+            console.log(paymentResponse);
+            if (paymentResponse.hasOwnProperty("action")) {
+              dropin.handleAction(paymentResponse.action);
+            } else {
+              if (paymentResponse.resultCode == "Authorised") {
+                dropin.setStatus('success', { message: 'Payment successful!' });
+              } else {
+                dropin.setStatus('error', { message: 'Something went wrong.'});
+              }
+            }
+            // Drop-in will handle the action object from the /payments response
+          })
+          .catch(error => {
+            throw Error(error);
+          });
+        },
+        onAdditionalDetails: (state, dropin) => {
+          makeDetailsCall(state.data)
+          // Your function calling your server to make a /payments/details request
+          .then(response => {
+            console.log(response);
+            if (response.hasOwnProperty("action")) {
+              dropin.handleAction(response.action);
+            } else {
+              if (response.resultCode == "Authorised") {
+                dropin.setStatus('success', { message: 'Payment successful!' });
+              } else {
+                dropin.setStatus('error', { message: 'Something went wrong.'});
+              }
+            }
+            // Drop-in will handle the action object from the /payments/details response
+          })
+          .catch(error => {
+            throw Error(error);
+          });
+        }
+      })
+      .mount('#dropin');
 
     });
   });
