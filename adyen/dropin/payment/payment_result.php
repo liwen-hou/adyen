cd4562d4feb5c275c1f9b563c41ab453330ea43f
liwen-hou <henryhouliwen@gmail.com>
2020-09-17 08:46:08 +0800

    result

--- adyen/dropin/payment/payment_result.php
+++ adyen/dropin/payment/payment_result.php
@@ -18,9 +18,14 @@
 </head>
 <body>
   <div>
-    <?php
-    require '../header.php';
-    ?>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal"><a href="../index.php" class="p-2 text-dark">Adyen DEMO</a></h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="cart.php"><i class="fas fa-cart-plus"></i> Cart</a>
+        <a class="p-2 text-dark" href="order.php"><i class="fas fa-user"></i> My Order</a>
+        <a class="p-2 text-dark" href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+      </nav>
+    </div>
     <div class="container">
       <div class="row">
           <div class="col-xs-6">
