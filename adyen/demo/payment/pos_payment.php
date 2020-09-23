daae6233786f1c11fc8bcb4978cf0bcb8e228a3c
liwen-hou <henryhouliwen@gmail.com>
2020-09-23 12:43:56 +0800

    qr

--- adyen/demo/payment/pos_payment.php
+++ adyen/demo/payment/pos_payment.php
@@ -18,39 +18,95 @@ try{
   if ( isset($shopperReference))  {
     $SaleToAcquirerData = "shopperReference=" . $shopperReference . "&recurringContract=ONECLICK,RECURRING";
     $TokenRequestedType = "Customer";
-  }
 
-  $request = array(
-    /** All order specific settings can be found in payment/Order.php */
+    $request = array(
+      /** All order specific settings can be found in payment/Order.php */
 
-    "SaleToPOIRequest"=> array(
-      "MessageHeader" => array(
-        "ProtocolVersion" => "3.0",
-        "MessageClass" => "Service",
-        "MessageCategory" => "Payment",
-        "MessageType" => "Request",
-        "ServiceID" => (string)$date->getTimestamp(),
-        "SaleID" => "LiwenDemoShop",
-        "POIID" => "V400m-346981680"
-      ),
-      "PaymentRequest" => array(
-        "SaleData" => array(
-          "SaleToAcquirerData" => $SaleToAcquirerData,
-          "TokenRequestedType" => $TokenRequestedType,
-          "SaleTransactionID" => array(
-            "TransactionID" => $reference,
-            "TimeStamp" =>  date("Y-m-d") . "T" . date("H:i:s+00:00")
-          )
+      "SaleToPOIRequest"=> array(
+        "MessageHeader" => array(
+          "ProtocolVersion" => "3.0",
+          "MessageClass" => "Service",
+          "MessageCategory" => "Payment",
+          "MessageType" => "Request",
+          "ServiceID" => (string)$date->getTimestamp(),
+          "SaleID" => "LiwenDemoShop",
+          "POIID" => "V400m-346981680"
         ),
-        "PaymentTransaction" => array(
-          "AmountsReq" => array(
-            "Currency" => "SGD",
-            "RequestedAmount" => 10
-          )
-        ),
+        "PaymentRequest" => array(
+          "SaleData" => array(
+            "SaleToAcquirerData" => $SaleToAcquirerData,
+            "TokenRequestedType" => $TokenRequestedType,
+            "SaleTransactionID" => array(
+              "TransactionID" => $reference,
+              "TimeStamp" =>  date("Y-m-d") . "T" . date("H:i:s+00:00")
+            )
+          ),
+          "PaymentTransaction" => array(
+            "AmountsReq" => array(
+              "Currency" => "SGD",
+              "RequestedAmount" => 10
+            )
+          ),
+        )
       )
-    )
-  );
+    );
+  } else {
+    {
+      $request  = [
+        "SaleToPOIRequest" => [
+          "MessageHeader" => [
+            "ProtocolVersion" => "3.0",
+            "MessageClass" => "Device",
+            "MessageCategory" => "Display",
+            "MessageType" => "Request",
+            "ServiceID" => (string)$date->getTimestamp(),
+            "SaleID" => "LiwenDemoShop", 
+            "POIID" => "V400m-346981680"
+          ],
+          "DisplayRequest" => [
+            "DisplayOutput" => [
+              [
+                "Device" => "CustomerDisplay",
+                "InfoQualify" => "Display",
+                "OutputContent" => [
+                  "OutputFormat" => "BarCode",
+                  "OutputBarcode" => [
+                    "BarcodeType" => "QRCode",
+                    "BarcodeValue" => "https://liwenhou.com/adyen/demo/"
+                    ]
+                    ]
+                  ],
+                  [
+                    "Device" => "CustomerDisplay",
+                    "InfoQualify" => "Display",
+                    "OutputContent" => [
+                      "OutputFormat" => "Text",
+                      "OutputText" => [
+                        [
+                          "Text" => "Get your M1 mobile app"
+                          ]
+                          ]
+                          ]
+                        ],
+                        [
+                          "Device" => "CustomerDisplay",
+                          "InfoQualify" => "Display",
+                          "OutputContent" => [
+                            "OutputFormat" => "Text",
+                            "OutputText" => [
+                              [
+                                "Text" => "Thank you!"
+                                ]
+                                ]
+                                ]
+                                ]
+                                ]
+                                ]
+                                ]
+                              ];
+};
+
+
 
   $data = json_encode($request);
   //  Initiate curl
