018ff0c30dca5dc7c5afa7c0ce6e0d958de5c2b4
Liwen Hou <henryhouliwen@gmail.com>
2021-06-27 10:02:38 +0800

    shopify

--- adyen/shopify/payments.php
+++ adyen/shopify/payments.php
@@ -260,7 +260,14 @@ date_default_timezone_set("Asia/Singapore");
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
     <script>
+      feather.replace()
+    </script>
+    <script>
+
+
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function () {
       'use strict'
