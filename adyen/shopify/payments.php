299669e25c45799e8ee1c4fd7b6a6ac688ec83a7
Liwen Hou <henryhouliwen@gmail.com>
2021-06-27 20:42:07 +0800

    shopify

--- adyen/shopify/payments.php
+++ adyen/shopify/payments.php
@@ -240,7 +240,7 @@ date_default_timezone_set("Asia/Singapore");
           $('#sellerInfo').append(html);
         }
 
-        html = '<br><a href="index.php?accountCode=' + accountCode + '" class="btn btn-primary float-right">Test Payments</a>';
+        html = '<br><a href="index.php?accountCode=' + accountCode + '" class="btn btn-primary float-right" style="margin-left: .8rem;">Test Payments</a>';
         $('#sellerInfo').append(html);
 
         if (!payoutStatus) {
