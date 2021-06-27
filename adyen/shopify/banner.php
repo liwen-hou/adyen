f39a9ffb50e4fafc6859bc9db0625956121aa12f
Liwen Hou <henryhouliwen@gmail.com>
2021-06-27 20:50:47 +0800

    shopify

--- adyen/shopify/banner.php
+++ adyen/shopify/banner.php
@@ -1,5 +1,4 @@
 <div class="py-5 text-center">
-  <img class="d-block mx-auto mb-4" src="img/adyen.png" alt="" width="72" height="72">
-  <h2><font color="#f8f9fa">Marketplace DEMO</font></h2>
+  <h2><font color="#f8f9fa">Liwen Shopify DEMO</font></h2>
   <h5><font color="#f8f9fa">Powered by Adyen for Platforms</font></h4>
 </div>
