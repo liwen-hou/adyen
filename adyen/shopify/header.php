80d4fe2cb8f429214957976fb3c9c1ee783a8966
Liwen Hou <henryhouliwen@gmail.com>
2021-06-27 18:48:40 +0800

    shopify

--- adyen/shopify/header.php
+++ adyen/shopify/header.php
@@ -1,5 +1,5 @@
 <div class="d-flex sticky-top flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm" style="opacity: 0.8;">
-<img src="https://cdn.shopify.com/shopifycloud/web/assets/v1/2217fb04df073033ccce8d125b0ea020.svg" style="width: 2.5rem; position: absolute; top: -1.5rem; left: -.2rem;">
+<img src="https://cdn.shopify.com/shopifycloud/web/assets/v1/2217fb04df073033ccce8d125b0ea020.svg" style="width: 2.5rem;">
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="index.php">Marketplace</a>
     <a class="p-2 text-dark" href="seller.php">Seller Center</a>
